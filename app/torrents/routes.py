3d92cfecf060756b7fd3f41b40d1252b0ebc6bbb
volcanonn <50715952+volcanonn@users.noreply.github.com>
2026-01-07 23:21:58 -0800

    it now runs

--- app/torrents/routes.py
+++ app/torrents/routes.py
@@ -3,7 +3,7 @@ from . import services, models
 
 router = APIRouter(prefix="/torrents")
 
-@router.post("/create/", response_model=models.User)
+@router.post("/create/") #, response_model=models.User
 def create_new_media(json_media: models.MediaInfoSummary):
     """
     Endpoint to create a new media.
@@ -11,7 +11,7 @@ def create_new_media(json_media: models.MediaInfoSummary):
     services.create_media_summary(json_media)
     
 
-@router.get("/users/{user_id}", response_model=models.User)
+'''@router.get("/users/{user_id}", response_model=models.User)
 def read_user(user_id: int):
     """
     Endpoint to get a user by their ID.
@@ -19,4 +19,4 @@ def read_user(user_id: int):
     db_user = services.get_user(user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    return db_user
+    return db_user'''
